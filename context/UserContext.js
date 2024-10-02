0a7fe317bb1e6088c307aaa8c8c3b29af340f99d
warathepj <warathepj@gmail.com>
2024-10-02 08:29:52 +0000

    mod usercontext

--- context/UserContext.js
+++ context/UserContext.js
@@ -1,20 +1,42 @@
+
+// context/UserContext.js
 "use client"
 import { createContext, useState, useEffect } from 'react';
 
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-    const [username, setUsername] = useState(() => {
+    const [username, setUsername] = useState(null);
+    // const [username, setUsername] = useState(() => {
         // Retrieve username from session storage if it exists
-        return sessionStorage.getItem('username') || '';
-    });
+        // return sessionStorage.getItem('username') || '';
+    // });
 
-    useEffect(() => {
-        // Store the username in session storage when it changes
-        if (username) {
-            sessionStorage.setItem('username', username);
+    //////////////////////
+
+    const loadUsernameFromLocalStorage = () => {
+        if (typeof window !== 'undefined') {
+          const storedUsername = localStorage.getItem("username");
+          if (storedUsername) {
+            setUsername(storedUsername);
+            // setUsername(JSON.parse(storedUsername));
+          }
         }
-    }, [username]);
+      };
+    
+      // Load user on component mount
+      useEffect(() => {
+        loadUsernameFromLocalStorage();
+      }, []);
+
+
+    ///////////////
+    // useEffect(() => {
+        // Store the username in session storage when it changes
+    //     if (username) {
+    //         sessionStorage.setItem('username', username);
+    //     }
+    // }, [username]);
     // console.log("UUUUUUUUUUUUUUUUUsername", username)
 
     return (
